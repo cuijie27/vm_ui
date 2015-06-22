d8a86ee022879dfa3247be28e5281e7cd6608477
Jun Tsai <jcai@ganshane.com>
2015-06-22 19:08:50 +0800

    添加API演示效果

--- js/Suowen.js
+++ js/Suowen.js
@@ -1,17 +1,36 @@
-/** suowen interface **/
+/*
+ * Copyright 2015 Suowen team.
+ */
+ /**
+  * suowen interface
+  **/
 (function(global) {
     "use strict";
 
     var ProtoBuf = dcodeIO.ProtoBuf;
     var SuowenAPI = {version:1,token:""};
+    /**
+     * 初始化API
+     * @param url API 的URL地址
+     */
     SuowenAPI.Init = function(url){
         this.url = url;
         this.builder = ProtoBuf.newBuilder({ convertFieldsToCamelCase: true });
-        //this.AddProto("protos/sys/Common.proto")
     };
+    /**
+     * 增加一个协议定义，目前尚不能增加多个协议.
+     * @see https://github.com/dcodeIO/ProtoBuf.js/issues/289
+     * TODO 能够支持添加多个协议
+     * @param path
+     * @constructor
+     */
     SuowenAPI.AddProto=function(path){
-        ProtoBuf.loadProtoFile(path,this.builder);
+        ProtoBuf.loadJsonFile(this.url+"/proto"+path,this.builder);
     };
+    /**
+     * 构建
+     * @returns 命名空间
+     */
     SuowenAPI.Build = function(){
         if(this.root){
             throw Error("Suowen Protocol Builted!");
@@ -21,6 +40,13 @@
         }
     };
 
+    /**
+     * 执行某一个API
+     * @param cmd API的命令
+     * @param protobufRequest protobuf请求对象
+     * @param success 当执行成功后的操作
+     * @param fail 当执行失败后的操作
+     */
     SuowenAPI.Fetch = function(cmd,protobufRequest,success,fail){
         if(this.url == null){
             throw Error("URL Not Configuration");
@@ -64,7 +90,7 @@
         var baseRequest= new this.root.sys.BaseRequest()
         baseRequest.setToken(this.token);
         baseRequest.setVersion(this.version);
-        baseRequest.set('.stock.TakeStockQueryRequest.cmd',protobufRequest)
+        baseRequest.set(cmd,protobufRequest)
 
         xhr.send(baseRequest.toArrayBuffer());
     };
