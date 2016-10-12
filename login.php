76be1543a5aed1b68fa882628082033a93d5e122
cuijie <cuijie@jeffc.cn>
2016-10-12 18:06:25 +0800

    simplify the codes in a simple function

--- login.php
+++ login.php
@@ -1,7 +1,5 @@
 <?php
-//https://v001.ganshane.com/ovirt-engine/sso/login.html
 require("./getVMInfo.php");
-require("./getHostInfo.php");
 require("./getCa.php");
 require("./getPswd.php");
 
@@ -11,40 +9,7 @@ $pswd = $_POST['password'];
 
 $userpwd_u = $usrName."@internal:".$pswd;
 
-$ret = getVMInfo($userpwd_u);
-
-$xml = new SimpleXMLElement($ret);
-$vms = $xml->xpath('/vms/vm');
-
-$vmArr = array();
-$i = 0;
-foreach($vms as $vm){
-	$vmName = $vm->name;
-	$vmId = $vm['id'];
-	$vmStatus = $vm->status;
-	$vmPort = $vm->display->secure_port;
-	$hostId = $vm->host['id'];
-
-	$vmArr[$i] = array();
-	$vmArr[$i]['vmName'] = $vmName;
-	$vmArr[$i]['vmId'] = $vmId;
-	$vmArr[$i]['status'] = $vmStatus;
-	$vmArr[$i]['port'] = $vmPort;
-	$vmArr[$i]['hostId'] = $hostId;
-
-	$retOfHost = getHostInfo($userpwd_u, $hostId);
-
-	$xml_h = new SimpleXMLElement($retOfHost);
-	$vms_h = $xml_h->xpath('/host');
-	foreach($vms_h as $vm_h){
-
-		$vmArr[$i]['organization'] = $vm_h->certificate->organization; 
-		$vmArr[$i]['subject'] = $vm_h->certificate->subject; 
-		$vmArr[$i]['address'] = $vm_h->address; 
-	}
-
-	$i++;
-}
+$vmArr = getVMInfo($userpwd_u);
 
 echo "<table border=1>";
 echo "<tr>";
@@ -62,9 +27,9 @@ echo "</table>";
 
 print_r($vmArr);
 
-getCa();
+//getCa();
 
-getPswd();
+//getPswd();
 
 
 ?>
