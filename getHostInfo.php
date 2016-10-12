2192d36e6aa7cd1a27dc011cda7601c60f2d9f5c
cuijie27 <49264664@qq.com>
2016-10-12 14:43:05 +0800

    add getHostInfo.php

---
+++ getHostInfo.php
@@ -0,0 +1,38 @@
+<?php
+$ch = curl_init($userpwd_u, $host_id);
+
+curl_setopt($ch, CURLOPT_VERBOSE, true);	// -v
+curl_setopt($ch, CURLOPT_USERPWD, $userpwd_u);	//-u
+//curl_setopt($ch, CURLOPT_USERPWD, "admin@internal:5idoris");	//-u
+//curl_setopt($ch, CURLOPT_CERTINFO, FALSE);		//-k
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);//-k
+curl_setopt($ch, CURLOPT_CAINFO,  getcwd().'/ca.pem'); 
+//CURLOPT_SSL_VERIFYPEER
+//CURLOPT_CAINFO
+//CURLOPT_CAPATH
+
+//curl_setopt($ch, CURLOPT_POST, true);		//-X
+//curl_setopt($ch, CURLOPT_POSTFIELDS, $xml_data);	//-X
+curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Content-Type: application/xml"));	//-H
+//curl_setopt($ch, CURLOPT_INFILE, $fh);		// -T
+curl_setopt($ch, CURLOPT_URL, "https://v001.ganshane.com/ovirt-engine/api/hosts/".$host_id);		// url
+
+//curl_setopt($ch, CURLOPT_URL, "https://v001.ganshane.com/ovirt-engine/api/vms");
+//curl_setopt($ch, CURLOPT_FILE, $fh);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$ret = curl_exec($ch);
+
+if($ret === false)
+{
+    echo 'Curl error: ' . curl_error($ch);
+}
+else
+{
+    echo '操作完成没有任何错误<br>';
+	//echo '返回结果为：'.$ret;
+}
+
+curl_close($ch);
+
+?>
